cadda4dde49482795d8465b47e9612022dcc99e7
ilukosiu <ilukosiu@LH7U0CND535737W>
2017-05-23 11:28:45 -0400

    Added summary of todo list

--- js/TodoController.js
+++ js/TodoController.js
@@ -26,6 +26,12 @@
     this.removeTodo = function (item, index) {
         this.list.splice(index, 1);
     }
+
+    this.getRemaining = function () {
+        return this.list.filter(function (item) {
+            return !item.completed;
+        })
+    }
 };
 
 angular
