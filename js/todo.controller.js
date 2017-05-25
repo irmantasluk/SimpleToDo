afe0cb8392221e1142df5db91efea1d545ccb345
ilukosiu <ilukosiu@LH7U0CND535737W>
2017-05-25 16:42:46 -0400

    adding $http calls

---
+++ js/todo.controller.js
@@ -0,0 +1,35 @@
+﻿function TodoController(TodoService) {
+
+    var vm = this;
+    vm.newTodo = '';
+    vm.list = [];
+
+    function getTodos() {
+        TodoService.retrieve().then(function (response) {
+            vm.list = response;
+        })
+    }
+
+    vm.addTodo = function () {
+        vm.list.unshift({
+                title: this.newTodo,
+                completed: false
+            });
+        vm.newTodo = '';
+    }
+    vm.removeTodo = function (item, index) {
+        vm.list.splice(index, 1);
+    }
+
+    vm.getRemaining = function () {
+        return vm.list.filter(function (item) {
+            return !item.completed;
+        })
+    }
+
+    getTodos();
+};
+
+angular
+    .module('app')
+    .controller('TodoController', TodoController);
---
+++ js/todo.service.js
@@ -0,0 +1,34 @@
+﻿function TodoService($http) {
+
+    var API = '//jsonplaceholder.typicode.com/todos/';
+
+    function create() {
+
+    }
+
+    function retrieve() {
+        return $http.get(API).then(function (response) {
+            return response.data.splice(0, 10);
+        })
+    }
+
+    function update() {
+
+    }
+
+    function remove() {
+
+    }
+
+    return {
+        create: create,
+        retrieve: retrieve,
+        update: update,
+        remove: remove
+    }
+
+}
+
+angular
+    .module('app')
+    .factory('TodoService', TodoService);
